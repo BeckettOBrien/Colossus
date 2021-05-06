e0e7a78efd9fdecd1fa11a04e894c2360e9f5fa5
Julz4455 <developer.julius0301@gmail.com>
2021-05-06 10:56:24 -0400

    stupid declarations

--- index.d.ts
+++ index.d.ts
@@ -1,50 +1,50 @@
 import http from 'http';
 
 // Global Colossus namespace
-declare namespace Colossus {
+declare module Colossus {
   // Declares types for brevity
   type Incoming = http.IncomingMessage;
   type Outgoing = http.ServerResponse;
   type MethodMapKeys = "uses" | "gets" | "heads" | "posts" | "puts" | "deletes" | "connects" | "options" | "traces" | "patches" | "catchall";
 
   /* Private classes ahead... but needed for type spec */
-  declare class Params {
+ class Params {
     request: Incoming;
     response: Outgoing;
 
-    async setRequest(r: Incoming);
-    async setResponse(r: Outgoing);
+    setRequest(r: Incoming);
+    setResponse(r: Outgoing);
   }
 
-  declare class Handlers {
-    async plugins();
-    async requestH();
+ class Handlers {
+    plugins();
+    requestH();
   }
 
   // Basic server class
-  export declare class Server {
+  export class Server {
     get req(): Function;
 
-    async catchall(f: Function);
-    async use(m: string, f: Function);
-    async get(m: string, f: Function);
-    async head(m: string, f: Function);
-    async post(m: string, f: Function);
-    async put(m: string, f: Function);
-    async delete(m: string, f: Function);
-    async connect(m: string, f: Function);
-    async options(m: string, f: Function);
-    async trace(m: string, f: Function);
+    catchall(f: Function);
+    use(m: string, f: Function);
+    get(m: string, f: Function);
+    head(m: string, f: Function);
+    post(m: string, f: Function);
+    put(m: string, f: Function);
+    delete(m: string, f: Function);
+    connect(m: string, f: Function);
+    options(m: string, f: Function);
+    trace(m: string, f: Function);
 
-    async start(port: number);
+    start(port: number);
   }
 
   // Plugins (middleware) class
-  export declare class Plugins {
-    async enable();
-    async disable();
-    async methodMap(): { [key: MethodMapKeys]: Function[] | Function };
-    async plugins();
-    async add(plugin: Function);
+  export class Plugins {
+    enable();
+    disable();
+    methodMap(): { [key in MethodMapKeys]: Function[] | Function };
+    plugins();
+    add(plugin: Function);
   }
 }
